fca083e19cbe193d4dd799bb6b4b68946d5454d8
Aics7 <issifu.alhassan@ashesi.edu.gh>
2019-03-14 21:14:38 +0000

    edited monitoring class

--- ICP_project1/src/Monitoring.java
+++ ICP_project1/src/Monitoring.java
@@ -1,134 +1,68 @@
 import java.util.*;
 
 /**
- 
  * This class holds information about all observatories
- 
  * @author Eugenia Mawuenya AKPO
- 
  * @author Michael PREMPEH
- 
  * @author Issifu ALHASSAN
- 
  * @author Francis AWEEENAGUA
- 
- * @version 1.0.3
- 
+ * @version 1.0.0
  */
-
-
 public class Monitoring {
-	
 	public static ArrayList<Observatory> observatories = new ArrayList<Observatory>();
-
-	
-	
 	/**
-	 
 	 * The default Constructor
-	 
 	 */
 	public Monitoring(){}
-	
-	
-	
 	/**
-	 
 	 * getObservatoryWithLargestAverage takes no parameters and returns the largest average recorded amongst the observatories
-	 
 	 * @return observatory
-	 
 	 */
-	
 	private static ArrayList<Double> largestAverage=new ArrayList<Double>();
-	
 	private static double highest=0;
-	
 	private static Observatory observatory;
-	
 	public static Observatory getObservatoryWithLargestAverage(){
 		for(Observatory i: observatories) {
-		   
 			largestAverage.add(i.averageGalamseyColourValueRecorded());
-		   
 			for(int j=0;j< largestAverage.size();j++) {if(largestAverage.get(j)>highest) {
-				
 					highest = largestAverage.get(j);
-			   
 			}
-		   
 			if(i.averageGalamseyColourValueRecorded()==highest) { observatory = i;}
-		   
 			}
 		}
 		//System.out.println(observatory);
-		
 		return observatory;
-		
 	}
-	
-	
-	
 	/**
-	 
 	 * getLargestGalamseyColourValueEverRecorded is a method that returns the largest Galamsey colour value ever recorded
-	 
 	 * @return largestGalamseyColourValue
-	 
 	 */
-    
-	
 	private static   ArrayList<Integer> list= new ArrayList<Integer>();
-	
 	private static   int largestGalamseyColourValue=0;
-	
 	public static int getLargestGalamseyColourValueEverRecorded(){
-		
 		for(Observatory i:observatories) {
-			
 			list.add(i.largestGalamseyColourValueRecorded());
-		
 			for (int j=0;j<list.size();j++) {
-			
 				if(list.get(j) > largestGalamseyColourValue) {
-				
 					largestGalamseyColourValue=list.get(j);
-			
 				}
-		
 			}
-
-     
 		}
-		
-		System.out.println(largestGalamseyColourValue);
-	    
-		return largestGalamseyColourValue;	   
-	
+		//System.out.println(largestGalamseyColourValue);
+		return largestGalamseyColourValue;
 	}
-	
-	
-	
 	/**
-	 
 	 * This method returns a list of all Galamsey whose vegetation colour value is greater than some arbitrary constant
-	 
 	 * @param number
-	 
 	 * @return List
-	 
 	 */
-   
-	
 	public static ArrayList<Galamsey> ListOfGalamseysWithColourValueGreaterThan(double number) {
 	  ArrayList<Galamsey> List=new ArrayList<Galamsey>(); 
 		for(Observatory i: observatories) {
 			List.addAll(i.galamseysWithColourValueGreaterThan(number));
              }
-		System.out.println(List);
+		//System.out.println(List);
 	    	   return List;
-	     
-		  
 	}
 
 	public static ArrayList<Galamsey> ListOfGalamseysWithColourValueLessThan(double number) {
@@ -136,23 +70,17 @@ public class Monitoring {
 		for(Observatory i: observatories) {
 			List.addAll(i.galamseysWithColourValueLesserThan(number));
 		}
-		System.out.println(List);
+		//System.out.println(List);
 		return List;
-
-
 	}
-
 	public static ArrayList<Galamsey> ListOfGalamseysWithColourValueEqualTo(double number) {
 		ArrayList<Galamsey> List=new ArrayList<Galamsey>();
 		for(Observatory i: observatories) {
 			List.addAll(i.galamseysWithColourValueEqualTo(number));
 		}
-		System.out.println(List);
+		//System.out.println(List);
 		return List;
-
-
 	}
-
 	/**
 	 * Checks the Observatory list for any observatory with a specified name.
 	 * @param nameOfObservatory a string of the name of observatory you want to check.
@@ -168,82 +96,40 @@ public class Monitoring {
 		}
 		return isItEqual;
 	}
-	
 	/**
-	 
 	 * Operations are executed in the main method
-	 
 	 * @param args
-	
 	 */
-	
 	public static void main(String[] args) {
 		Monitoring monitor =new Monitoring();
-		
 		Galamsey g = new Galamsey ("brown",3);
-       
 		Galamsey q = new Galamsey ("brown",3);
-       
 		Galamsey r = new Galamsey ("yellow",2);
-       
 		Galamsey y = new Galamsey ("green",1);
-       
 		Galamsey u = new Galamsey ("green",1);
-       
 		Galamsey p = new Galamsey ("brown",3);
-       
 		Galamsey a = new Galamsey ("green",1);
-      
 		Galamsey s = new Galamsey ("yellow",2);
-        
 		Galamsey d = new Galamsey ("green",1);
-       
 		Galamsey h = new Galamsey ("brown",3);
-      
 		ArrayList<Galamsey> events =new ArrayList<Galamsey>();
-       
 		ArrayList<Galamsey> event =new ArrayList<Galamsey>();
-       
 		events.add(g);
-       
 		events.add(q);
-      
 		events.add(r);
-       
 		event.add(y);
-       
 		events.add(u);
-       
 		event.add(p);
-       
 		events.add(a);
-       
 		event.add(s);
-      
 		events.add(d);
-       
 		event.add(h);
-     
-	
-       
 		Observatory e = new Observatory("force","Ghana",2000, 50000,events);
-		
         Observatory t = new Observatory("force2","Togo",3000, 100000,event);
-	    
-		
-		
 	    monitor.observatories.add(e);
-	   
 		monitor.observatories.add(t);
-	   
 		monitor.getObservatoryWithLargestAverage();
-	   
 		monitor.getLargestGalamseyColourValueEverRecorded();
-	   
 		monitor.ListOfGalamseysWithColourValueGreaterThan(0);
-        
-
 	}
- 
 }
-
